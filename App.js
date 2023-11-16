d86751f7b01f7c01b6acaa2ec7ea540ccd48a6b2
GVIM0 <guimo.enterprise@gmail.com>
2023-11-15 18:43:37 -0600

    Actualización

--- App.js
+++ App.js
@@ -1,10 +1,103 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+        const { cameraStatus } = await ImagePicker.requestCameraPermissionsAsync();
+        if (cameraStatus !== 'granted') {
+          alert('Sorry, we need camera permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const takePicture = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const handleChangeImage = () => {
+    Alert.alert(
+      'Cambiar Imagen',
+      'Elige una opción:',
+      [
+        { text: 'Seleccionar de la Galería', onPress: pickImage },
+        { text: 'Tomar una Foto', onPress: takePicture },
+        { text: 'Cancelar', style: 'cancel' },
+      ],
+      { cancelable: true }
+    );
+  };
+
+  const handleLogin = () => {
+    if (username.trim() !== '' && password.trim() !== '') {
+      Alert.alert('Usuario Registrado', 'Gracias por tu registro');
+    } else {
+      Alert.alert('Los campos no pueden estar vacios', 'Por favor rellena los campos');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Inicio de Sesión</Text>
+      <Image
+        source={{ uri: image ? image : 'default-avatar-photo-placeholder-profile-icon-vector' }}
+        style={styles.image}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleChangeImage}>
+        <Text style={styles.buttonText}>Cambiar Imagen</Text>
+      </TouchableOpacity>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+        placeholderTextColor="#fff"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry
+        placeholderTextColor="#fff"
+      />
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +106,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0E1111',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginVertical: 20,
+    borderColor: '#18FF33',
+    borderWidth: 2,
+  },
+  button: {
+    backgroundColor: '#18FF33',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  loginButton: {
+    backgroundColor: '#18FF33',
+    padding: 15,
+    borderRadius: 8,
+    marginVertical: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: '#18FF33',
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    color: '#fff',
   },
 });
